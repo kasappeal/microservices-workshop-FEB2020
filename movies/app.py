d6e179d4b319f55f99bdb9be434a31665492c043
Alberto Casero <alberto@kasfactory.net>
2020-02-20 14:10:42 +0100

    Endpoint de detalle de película y actualización de rating

--- movies/app.py
+++ movies/app.py
@@ -1,3 +1,5 @@
+from bson import ObjectId
+from bson.errors import InvalidId
 from flask import Flask, request, jsonify
 from flask_jwt import jwt_required, current_identity
 
@@ -19,3 +21,46 @@ def movies_list():
             'title': result['title']  # mejor hacerlo así: result.get('title')
         })
     return jsonify(movies)
+
+
+@app.route('/movies/<string:movie_id>')
+def movie_detail(movie_id):
+    try:
+        result = db_client.movies.find_one({'_id': ObjectId(movie_id)})
+    except InvalidId:
+        result = None
+    if result is None:
+        return jsonify({'error': 'Movie not found'}), 404
+    movie = {
+        'id': str(result['_id']),
+        'title': result['title'], # mejor hacerlo así: result.get('title')
+        'description': result['description'],
+        'rating': result['rating'],
+        'review_count': result['review_count']
+    }
+    return jsonify(movie)
+
+
+@app.route('/movies/<string:movie_id>', methods=['POST'])
+def update_movie_rating(movie_id):
+    try:
+        movie = db_client.movies.find_one({'_id': ObjectId(movie_id)})
+    except InvalidId:
+        movie = None
+    if movie is None:
+        return jsonify({'error': 'Movie not found'}), 404
+    data = request.json
+    if movie['rating'] is None:
+        movie['rating'] = 0
+    current_rating = movie['rating'] * movie['review_count']
+    current_rating += data['rating']
+    movie['review_count'] += 1
+    new_rating = current_rating / movie['review_count']
+    db_client.movies.update_one(
+        {'_id': ObjectId(movie_id)},
+        {'$set': {
+            'rating': new_rating,
+            'review_count': movie['review_count']
+        }}
+    )
+    return jsonify({'result': 'Movie updated'})
